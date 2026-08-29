2face2a7b8a3d6b5522d7e35dd637cf5e2027105
Jonathan <jonathan.houze+git@gmail.com>
2026-08-29 10:30:04 +0800

    fix(lsp/neocmake): add cmd, mason install, and enable

--- lua/plugins/lang/cmake.lua
+++ lua/plugins/lang/cmake.lua
@@ -1,4 +1,17 @@
 -- CMake language support (treesitter + LSP config).
-vim.lsp.config('neocmake', {})
+require('utils').install_with_mason {
+	'neocmakelsp',
+}
+
+vim.lsp.config('neocmake', {
+	cmd = { 'neocmakelsp', 'stdio' },
+	filetypes = { 'cmake' },
+	root_markers = { '.neocmake.toml', '.git', 'build', 'cmake' },
+})
+
 local lint = require("lint")
 lint.linters_by_ft.cmake = { "cmakelint" }
+
+vim.lsp.enable 'neocmake'
+
+-- vim: ts=2 sts=2 sw=2 et
