0868c5261c4b731eaa8b2c697d2a5fd30bd55280
Jonathan <jonathan.houze+git@gmail.com>
2026-08-29 10:23:32 +0800

    fix(lsp/taplo): add cmd, mason install, and enable

--- lua/plugins/lang/toml.lua
+++ lua/plugins/lang/toml.lua
@@ -1,2 +1,14 @@
 -- TOML language support (treesitter + LSP config).
-vim.lsp.config('taplo', {})
+require('utils').install_with_mason {
+	'taplo',
+}
+
+vim.lsp.config('taplo', {
+	cmd = { 'taplo', 'lsp', 'stdio' },
+	filetypes = { 'toml' },
+	root_markers = { '.taplo.toml', 'taplo.toml', '.git' },
+})
+
+vim.lsp.enable 'taplo'
+
+-- vim: ts=2 sts=2 sw=2 et
