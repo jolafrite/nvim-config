d04bd30693d679911f00687125d0b68047996359
Jonathan <jonathan.houze+git@gmail.com>
2026-08-29 12:55:24 +0800

    feat: add crates plugin for rust

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,37 +1,33 @@
--- Rust: rust_analyzer (LSP) + rustfmt conform + clippy lint.
---
--- `rustfmt` and `clippy` are rustup components, not mason tools, so they are
--- not in mason.lua/mason-lspconfig.lua. Install with:
---   rustup component add rustfmt clippy
-require('utils').install_with_mason {
-	'rust-analyzer',
+local gh = require 'utils'
+
+vim.pack.add {
+  gh 'Saecki/crates.nvim',
 }
 
-vim.lsp.config('rust_analyzer', {
-	cmd = { 'rust-analyzer' },
-	filetypes = { 'rust' },
-	root_markers = { 'Cargo.toml', '.git' },
+require('utils').install_with_mason {
+  'rust-analyzer',
+}
+
+vim.vim.lsp.config('rust_analyzer', {
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },
+  root_markers = { 'Cargo.toml', 'rust-project.json' },
 })
 
 vim.lsp.enable 'rust_analyzer'
 
-require("conform").formatters_by_ft.rust = { "rustfmt" }
+require('Saecki/crates.nvim').setup()
 
-require("conform").formatters.rustfmt = {
-	command = "rustfmt",
-	stdin = true,
-	args = { "--emit=stdout" },
+require('conform').formatters_by_ft.rust = { 'rustfmt' }
+
+require('conform').formatters.rustfmt = {
+  command = 'rustfmt',
+  stdin = true,
+  args = { '--emit=stdout' },
 }
 
-require("lint").linters_by_ft.rust = { "clippy" }
+require('lint').linters_by_ft.rust = { 'clippy' }
 
--- Overridden with ignore_exitcode=true: cargo clippy exits 101 when the code
--- doesn't compile, which is expected (can't lint uncompilable code). Without
--- the override nvim-lint reports "Linter command `cargo` exited with code: 101".
-require("lint").linters.clippy = vim.tbl_deep_extend(
-	"force",
-	require("lint").linters.clippy,
-	{ ignore_exitcode = true }
-)
+require('lint').linters.clippy = vim.tbl_deep_extend('force', require('lint').linters.clippy, { ignore_exitcode = true })
 
 -- vim: ts=2 sts=2 sw=2 et
