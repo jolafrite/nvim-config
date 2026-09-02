3de961f0ce0df4fc4aa6196e93b41dfd5d16b786
Jonathan <jonathan.houze+git@gmail.com>
2026-09-02 17:22:49 +0800

    fix: remove plugins init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,8 @@ _G.Utils = require 'utils'
 require 'config.options'
 require 'config.keymaps'
 require 'config.autocmds'
-require 'utils.package_manager'
-require 'plugins'
+
+PluginsLoader.load 'plugins'
+PackageManager.load_all()
 
 pcall(vim.cmd.colorscheme, 'shades-of-purple')
