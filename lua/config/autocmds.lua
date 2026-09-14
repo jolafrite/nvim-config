dc116b981b19977cca827bd547d3a382db4afdc7
Jonathan <jolafrite@users.noreply.github.com>
2026-09-14 11:31:17 +0800

    fix: errors after migration to nvim 0.13 (#57)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,24 +1,43 @@
+-- 1. Automatically reload files changed outside of Neovim
 local autoreload = vim.api.nvim_create_augroup('autoreload', { clear = true })
 vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   group = autoreload,
   callback = function()
-    if vim.o.buftype ~= 'nofile' then vim.cmd.checktime() end
+    if vim.bo.buftype ~= 'nofile' then vim.cmd.checktime() end
   end,
 })
 
+-- 2. General FileType-specific settings
+local filetype_settings = vim.api.nvim_create_augroup('filetype_settings', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
+  group = filetype_settings,
   pattern = { 'json', 'jsonc', 'markdown' },
-  callback = function() vim.opt.conceallevel = 0 end,
-})
-
-vim.api.nvim_create_autocmd('BufEnter', {
   callback = function()
-    vim.cmd 'set formatoptions-=cro'
-    vim.cmd 'setlocal formatoptions-=cro'
+    vim.opt_local.conceallevel = 0
   end,
 })
 
+-- 3. Persistent cleanup of formatoptions
+vim.api.nvim_create_autocmd('FileType', {
+  group = filetype_settings,
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove({ 'c', 'r', 'o' })
+  end,
+})
+
+-- 4. Highlight text actions (Yank & Put)
+vim.api.nvim_create_autocmd({ 'TextYankPost', 'TextPutPost' }, {
+  group = vim.api.nvim_create_augroup('hl_actions', { clear = true }),
+  callback = function()
+    vim.hl.hl_op({ higroup = 'IncSearch', timeout = 150 })
+  end,
+})
+
+-- 5. Smart Cursorline management (Active at rest, disabled during actions)
+local cursorline_toggle = vim.api.nvim_create_augroup('cursorline_toggle', { clear = true })
 vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+  group = cursorline_toggle,
   callback = function()
     if vim.w.auto_cursorline then
       vim.wo.cursorline = true
@@ -27,6 +46,7 @@ vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
   end,
 })
 vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+  group = cursorline_toggle,
   callback = function()
     if vim.wo.cursorline then
       vim.w.auto_cursorline = true
@@ -35,31 +55,21 @@ vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('yank_highlight', { clear = true }),
+-- 6. Optimized Relative Line Number Toggling
+local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+  group = numbertoggle,
   callback = function()
-    local ok, hl_op = pcall(require, 'vim.hl')
-    if ok and type(hl_op.hl_op) == 'function' then
-      pcall(hl_op.hl_op, 0, vim.fn.getregion('"', 'v'), true)
+    if vim.wo.number and vim.api.nvim_get_mode().mode ~= 'i' then
+      vim.wo.relativenumber = true
     end
   end,
 })
-
-local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
-  pattern = '*',
-  group = numbertoggle,
-  callback = function()
-    if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then vim.opt.relativenumber = true end
-  end,
-})
 vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
-  pattern = '*',
   group = numbertoggle,
   callback = function()
-    if vim.o.nu then
-      vim.opt.relativenumber = false
-      vim.cmd.redraw()
+    if vim.wo.number then
+      vim.wo.relativenumber = false
     end
   end,
 })
